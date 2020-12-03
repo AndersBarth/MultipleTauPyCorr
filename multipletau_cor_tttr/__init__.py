c0c1ba299279b5b1f26085a2fde6482d51a99606
Anders <anders.barth@gmail.com>
2020-12-03 13:15:00 +0100

    Fixed platform-dependent naming of compiled file. Added example as submodule.

--- multipletau_cor_tttr/__init__.py
+++ multipletau_cor_tttr/__init__.py
@@ -1,2 +1,2 @@
 __author__ = "Anders Barth"
-__version__ = "0.1.0"
+__version__ = "0.1.1"
--- setup.py
+++ setup.py
@@ -1,7 +1,28 @@
 from distutils.core import setup, Extension
 
-module = Extension('multipletau_cor_tttr/CCF',
-                   sources=['multipletau_cor_tttr/CCF.c'])
+#  https://stackoverflow.com/questions/4529555/building-a-ctypes-based-c-library-with-distutils
+from distutils.command.build_ext import build_ext as build_ext_orig
+class CTypesExtension(Extension): pass
+
+class build_ext(build_ext_orig):
+
+    def build_extension(self, ext):
+        self._ctypes = isinstance(ext, CTypesExtension)
+        return super().build_extension(ext)
+
+    def get_export_symbols(self, ext):
+        if self._ctypes:
+            return ext.export_symbols
+        return super().get_export_symbols(ext)
+
+    def get_ext_filename(self, ext_name):
+        if self._ctypes:
+            return ext_name + '.so'
+        return super().get_ext_filename(ext_name)
+
+
+module = CTypesExtension('multipletau_cor_tttr/CCF',
+                         sources=['multipletau_cor_tttr/CCF.c'])
 
 setup(
     name='multipletau_cor_tttr',
@@ -10,7 +31,7 @@ setup(
     license='MIT',
     author_email='anders.barth@gmail.com',
     packages=['multipletau_cor_tttr'],
-    package_data={'multipletau_cor_tttr': ['sample/sample_data.npy', 'sample/multipletau_cor_tttr_example.py']},
+    package_data={'multipletau_cor_tttr': ['sample_data.npy']},
     url='http://testpypi.python.org/pypi/multipletau_cor_tttr/',
     description='Library for correlation of time-tagged time-resolved photon data for'
                 ' fluorescence correlation spectroscopy (FCS) analysis.',
@@ -19,5 +40,6 @@ setup(
     install_requires=['numpy', 'matplotlib'],
     classifiers=['Programming Language :: Python :: 2.7',
                  'Programming Language :: Python :: 3.5'],
+    cmdclass={'build_ext': build_ext},
     keywords=['FCS Fluorescence Correlation Spectroscopy Single Photon Counting TTTR Multiple-tau algorithm']
 )
